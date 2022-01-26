8aa5de6580b556c7f83eb5a5f35eafd8335d8d6f
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2022-01-26 11:53:55 +0200

    perzaliv

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+const listOfCountriesEl = document.querySelector(".country-list");
+const countryInfoEl = document.querySelector(".country-info");
+
+export function fetchCountry(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    }
+  );
+}
